4a2127eec53d7f50cf6d02c046c186e124120066
Luke1550 <slodresko@gmail.com>
2022-08-21 21:30:49 +0200

    finished everything up

--- src/main/java/cc/tweaked_programs/cccbridge/block/peripherals/TrainPeripheral.java
+++ src/main/java/cc/tweaked_programs/cccbridge/block/peripherals/TrainPeripheral.java
@@ -4,6 +4,7 @@ import com.simibubi.create.content.logistics.trains.entity.Train;
 import com.simibubi.create.content.logistics.trains.management.edgePoint.station.StationEditPacket;
 import com.simibubi.create.content.logistics.trains.management.edgePoint.station.StationTileEntity;
 import com.simibubi.create.content.logistics.trains.management.edgePoint.station.TrainEditPacket;
+import com.simibubi.create.content.logistics.trains.management.schedule.Schedule;
 import com.simibubi.create.foundation.networking.AllPackets;
 import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.IComputerAccess;
@@ -11,7 +12,6 @@ import dan200.computercraft.api.peripheral.IPeripheral;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,18 +22,17 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class TrainPeripheral implements IPeripheral {
-
     private final List<IComputerAccess> connectedComputers = new ArrayList<>();
     private final BlockPos pos;
     private final Level level;
     private final StationTileEntity station;
+    private static Schedule schedule;
 
     public TrainPeripheral(@NotNull BlockPos pos, Level level) {
         this.pos = pos;
         this.level = level;
         this.station = (StationTileEntity) level.getBlockEntity(pos);
     }
-
 
     @NotNull
     @Override
@@ -46,54 +45,61 @@ public class TrainPeripheral implements IPeripheral {
         connectedComputers.remove(computer);
     }
 
-    private Block get_block() {
-        return this.level.getBlockEntity(pos).getBlockState().getBlock();
-    }
-
+    //assembles the train
     @LuaFunction
     public boolean assemble() {
+        if (station.getStation().getPresentTrain() != null) {
+            return false;
+        }
         if (station.tryEnterAssemblyMode()) {
             station.assemble(UUID.fromString("069a79f4-44e9-4726-a5be-fca90e38aaf5"));
-            //assemble stuff here
+            station.tick();
+            if (this.schedule == null) {
+                return true;
+            }
+            station.getStation().getPresentTrain().runtime.setSchedule(this.schedule, true);
+            this.schedule = null;
             return true;
         }
         return false;
     }
 
+    //disassembles the train
     @LuaFunction
     public boolean disassemble() {
+        if (station.getStation().getPresentTrain() == null) {
+            return false;
+        }
         if (station.getStation().getPresentTrain().canDisassemble()) {
             Direction direction = station.getAssemblyDirection();
             BlockPos position = station.edgePoint.getGlobalPosition().above();
+            this.schedule = station.getStation().getPresentTrain().runtime.getSchedule();
             station.getStation().getPresentTrain().disassemble(direction, position);
             return true;
         }
         return false;
     }
 
+    //returns the stations name
     @LuaFunction
     public String getStationName() {
         return station.getStation().name;
     }
 
-    @LuaFunction
-    public boolean isAssembled() {
-        if (station.getStation().getPresentTrain() == null) {
-            return false;
-        } else return station.getStation().getPresentTrain().canDisassemble();
-    }
-
+    //returns the train's name
     @LuaFunction
     public String getTrainName() {
         return Objects.requireNonNull(station.getStation().getPresentTrain()).name.getContents();
     }
 
+    //sets the Stations name
     @LuaFunction
     public boolean setStationName(@NotNull String name) {
         AllPackets.channel.sendToServer(StationEditPacket.configure(station.getBlockPos(), false, name));
         return true;
     }
 
+    //sets the Trains name
     @LuaFunction
     public boolean setTrainName(@NotNull String name) {
         if (station.getStation().getPresentTrain() == null) {
@@ -103,9 +109,29 @@ public class TrainPeripheral implements IPeripheral {
         AllPackets.channel.sendToServer(new TrainEditPacket(train.id, name, train.icon.getId()));
         return true;
     }
-    /**
-     * Will be called when a computer connects to our block
-     */
+
+    //gets the Number of Bogeys atteched to the Train
+    @LuaFunction
+    public int getBogeys() {
+        if (station.getStation().getPresentTrain() == null) {
+            return 0;
+        }
+        return station.getStation().getPresentTrain().carriages.size();
+    }
+
+    //gets if there is a train present
+    @LuaFunction
+    public boolean getPresentTrain() {
+        return station.getStation().getPresentTrain() != null;
+    }
+
+    //Clears the schedule saved in the station
+    @LuaFunction
+    public boolean clearSchedule() {
+        this.schedule = null;
+        return true;
+    }
+
     @Override
     public void attach(@Nonnull IComputerAccess computer) {
         connectedComputers.add(computer);
