8daa398e21a2318b94202e52f792290c92f5d974
Luke1550 <slodresko@gmail.com>
2022-08-31 20:32:55 +0200

    try to merge correctly

--- src/main/java/cc/tweaked_programs/cccbridge/block/peripherals/TrainPeripheral.java
+++ src/main/java/cc/tweaked_programs/cccbridge/block/peripherals/TrainPeripheral.java
@@ -1,9 +1,11 @@
 package cc.tweaked_programs.cccbridge.block.peripherals;
 
+import com.simibubi.create.Create;
+import com.simibubi.create.content.logistics.trains.GraphLocation;
 import com.simibubi.create.content.logistics.trains.entity.Train;
-import com.simibubi.create.content.logistics.trains.management.edgePoint.station.StationEditPacket;
+import com.simibubi.create.content.logistics.trains.management.edgePoint.station.GlobalStation;
 import com.simibubi.create.content.logistics.trains.management.edgePoint.station.StationTileEntity;
-import com.simibubi.create.content.logistics.trains.management.edgePoint.station.TrainEditPacket;
+import com.simibubi.create.content.logistics.trains.management.edgePoint.station.TrainEditPacket.TrainEditReturnPacket;
 import com.simibubi.create.content.logistics.trains.management.schedule.Schedule;
 import com.simibubi.create.foundation.networking.AllPackets;
 import dan200.computercraft.api.lua.LuaFunction;
@@ -12,7 +14,9 @@ import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+
 
 public class TrainPeripheral implements IPeripheral {
     private final List<IComputerAccess> connectedComputers = new ArrayList<>();
@@ -96,22 +101,42 @@ public class TrainPeripheral implements IPeripheral {
     //sets the Stations name
     @LuaFunction
     public final boolean setStationName(@NotNull String name) {
-        AllPackets.channel.sendToServer(StationEditPacket.configure(station.getBlockPos(), false, name));
-        return true;
+        GlobalStation station2 = station.getStation();
+        GraphLocation graphLocation = station.edgePoint.determineGraphLocation();
+        if (station2 != null && graphLocation != null) {
+            station2.name = name;
+            Create.RAILWAYS.sync.pointAdded(graphLocation.graph, station2);
+            Create.RAILWAYS.markTracksDirty();
+            station.notifyUpdate();
+            return true;
+        }
+        //AllPackets.channel.sendToServer(StationEditPacket.configure(station.getBlockPos(),false,name));
+        return false;
     }
 
-    //sets the Trains name
+    //sets the Trains
     @LuaFunction
-    public final boolean setTrainName(@NotNull String name) {
+    public final MethodResult setTrainName(@NotNull String name) {
         if (station.getStation().getPresentTrain() == null) {
-            return false;
+            return MethodResult.of(false, "There is no train to set the name of");
         }
         Train train = station.getStation().getPresentTrain();
-        AllPackets.channel.sendToServer(new TrainEditPacket(train.id, name, train.icon.getId()));
-        return true;
+        Train Train = Create.RAILWAYS.sided(level).trains.get(train.id);
+        if (Train == null) {
+            return MethodResult.of(false, "Train not found");
+        }
+        if (!name.isBlank()) {
+            Train.name = new TextComponent(name);
+            ;
+            station.tick();
+            AllPackets.channel.send(PacketDistributor.ALL.noArg(), new TrainEditReturnPacket(Train.id, name, Train.icon.getId()));
+            return MethodResult.of(true, "Train name set to" + name);
+        }
+        //AllPackets.channel.sendToServer(new TrainEditPacket(train.id, name, train.icon.getId()));
+        return MethodResult.of(false, "Train name cannot be blank");
     }
 
-    //gets the Number of Bogeys atteched to the Train
+    //gets the Number of Bogeys attached to the Train
     @LuaFunction
     public final int getBogeys() {
         if (station.getStation().getPresentTrain() == null) {
