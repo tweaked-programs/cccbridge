8e0d44b9bb4182de6dfd5d871eceb3998f5d09d6
Sammy <SammyKoch@pm.me>
2023-07-08 23:17:59 +0200

    v1.5.1 fix server crash on fabric

--- fabric/src/main/java/cc/tweaked_programs/cccbridge/CCCBridgeClient.java
+++ fabric/src/main/java/cc/tweaked_programs/cccbridge/CCCBridgeClient.java
@@ -4,8 +4,14 @@ import cc.tweaked_programs.cccbridge.blockEntity.AnimatronicBlockEntity;
 import cc.tweaked_programs.cccbridge.blockEntity.RedRouterBlockEntity;
 import cc.tweaked_programs.cccbridge.blockEntityRenderer.AnimatronicBlockEntityRenderer;
 import cc.tweaked_programs.cccbridge.blockEntityRenderer.RedRouterBlockEntityRenderer;
+import cc.tweaked_programs.cccbridge.entity.animatronic.AnimatronicEntity;
+import cc.tweaked_programs.cccbridge.entity.animatronic.AnimatronicModel;
+import cc.tweaked_programs.cccbridge.entity.animatronic.AnimatronicRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class CCCBridgeClient implements ClientModInitializer {
@@ -13,5 +19,10 @@ public class CCCBridgeClient implements ClientModInitializer {
     public void onInitializeClient() {
         BlockEntityRendererRegistry.register((BlockEntityType<AnimatronicBlockEntity>)CCCRegister.getBlockEntityType("animatronic_block"), AnimatronicBlockEntityRenderer::new);
         BlockEntityRendererRegistry.register((BlockEntityType<RedRouterBlockEntity>)CCCRegister.getBlockEntityType("redrouter_block"), RedRouterBlockEntityRenderer::new);
+
+        // Register Animatronic Entity rendering stuff
+        EntityType<AnimatronicEntity> entity = (EntityType<AnimatronicEntity>)CCCRegister.getEntityType("animatronic");
+        EntityRendererRegistry.register(entity, AnimatronicRenderer::new);
+        EntityModelLayerRegistry.registerModelLayer(AnimatronicModel.LAYER_LOCATION, AnimatronicModel::createBodyLayer);
     }
 }
