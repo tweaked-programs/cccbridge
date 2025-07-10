fff73febdc77e2b92161fb61d89b3026c065d54e
Sammy <SammyKoch@pm.me>
2025-07-10 22:32:30 +0200

    fixed server crash (oopsie)

--- neoforge/src/main/java/dev/kleinbox/cccbridge/common/modloader/CCCBridge.java
+++ neoforge/src/main/java/dev/kleinbox/cccbridge/common/modloader/CCCBridge.java
@@ -35,13 +35,13 @@ public class CCCBridge {
     //public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public CCCBridge(IEventBus eventBus) {
-        var ignored = CCConfig.CONFIG; // Just needs to be touched soon-ish for mcqoy to work
         CCCRegistries.register(eventBus);
     }
 
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
     public static void clientInit(FMLClientSetupEvent event) {
+        var ignored = CCConfig.CONFIG; // Just needs to be touched soon-ish for mcqoy to work
         if (ModList.get().isLoaded("mcqoy"))
             return;
 
